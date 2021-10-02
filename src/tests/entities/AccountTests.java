892f7cb5618f14cff4c343670a2c1b107f48de04
Matheus M. Jeronimo <matheus.martins.jeronimo@gmail.com>
2021-10-02 13:18:07 -0300

    Add fullWithDraw test

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -37,4 +37,19 @@ public class AccountTests {
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
+
+	@Test
+	public void fullWithDrawShouldClearBalance() {
+		// We do not put "when" because there's only one scenario in this case.
+		
+		double expectedValue = 0.0;
+		double initialBalance = 899.21;
+		Account acc = AccountFactory.createAccount(initialBalance);
+		
+		double result = acc.fullWithdraw();
+		
+		// You can have as many assertions as you want
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertEquals(result, initialBalance);
+	}
 }
